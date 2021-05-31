aee3fc2cec83fd4f764222b4207607774576ccc2
Rafael Campos <rafaelscampos15@gmail.com>
2021-05-31 17:04:12 -0300

    Fix: Removed keyframes errors.

--- src/components/Footer/styles/animations.ts
+++ src/components/Footer/styles/animations.ts
@@ -1,23 +1,23 @@
 import { keyframes } from "styled-components";
 
 export const footerIconAnimation = keyframes`
-  100% {
+  \ 100% {
     transform:translateX(0);
     transform-origin:50% 50%
   }
-  15% {
+  \ 15% {
     transform:translateX(-5px) rotate(-6deg)
   }
-  30% {
+  \ 30% {
     transform:translateX(12px) rotate(6deg)
   }
-  45% {
+  \ 45% {
     transform:translateX(-6px) rotate(-3.6deg)
   }
-  60% {
+  \ 60% {
     transform:translateX(6px) rotate(2.4deg)
   }
-  75% {
+  \ 75% {
     transform:translateX(-6px) rotate(-1.2deg)
   }
 `
--- src/components/Menu/styles/animations.ts
+++ src/components/Menu/styles/animations.ts
@@ -1,12 +1,12 @@
 import { keyframes } from "styled-components";
 
 export const glassInAnimation = keyframes`
-  0% {
+  \ 0% {
     -webkit-transform: translateX(-50px);
       transform: translateX(-50px);
     opacity: 0;
   }
-  100% {
+  \ 100% {
     -webkit-transform: translateX(0);
       transform: translateX(0);
     opacity: 1;
@@ -14,53 +14,53 @@ export const glassInAnimation = keyframes`
 `
 
 export const glassIconAnimation = keyframes`
-  100% {
+  \ 100% {
     -webkit-transform:translateX(0);
     transform:translateX(0);
     -webkit-transform-origin:50% 50%;
     transform-origin:50% 50%;
   }
-  15% {
+  \ 15% {
     -webkit-transform:translateX(-12px) rotate(-6deg);
     transform:translateX(-12px) rotate(-6deg);
   }
-  30% {
+  \ 30% {
     -webkit-transform:translateX(23px) rotate(6deg);
     transform:translateX(23px) rotate(6deg);
   }
-  45% {
+  \ 45% {
     -webkit-transform:translateX(-13px) rotate(-3.6deg);
     transform:translateX(-13px) rotate(-3.6deg);
   }
-  60% {
+  \ 60% {
     -webkit-transform:translateX(15px) rotate(2.4deg);
     transform:translateX(15px) rotate(2.4deg);
   }
-  75% {
+  \ 75% {
     -webkit-transform:translateX(-12px) rotate(-1.2deg);
     transform:translateX(-12px) rotate(-1.2deg);
   }
 `
 
 export const logoAnimation = keyframes`
-  50%,
-  100% {
+  \ 50%,
+  \ 100% {
     opacity: 1;
   }
-  25%,
-  75% {
+  \ 25%,
+  \ 75% {
     opacity: 0;
   }
 
 `
 
 export const iconIn = keyframes`
-  0% {
+  \ 0% {
     -webkit-transform: translateZ(-1400px);
       transform: translateZ(-1400px);
     opacity: 0;
   }
-  100% {
+  \ 100% {
     -webkit-transform: translateZ(0);
       transform: translateZ(0);
     opacity: 1;
@@ -68,12 +68,12 @@ export const iconIn = keyframes`
 `
 
 export const iconOut = keyframes`
-  0% {
+  \ 0% {
     -webkit-transform: scale(1);
       transform: scale(1);
     opacity: 1;
   }
-  100% {
+  \ 100% {
     -webkit-transform: scale(0);
       transform: scale(0);
     opacity: 1;
@@ -81,12 +81,12 @@ export const iconOut = keyframes`
 `
 
 export const slideInTop = keyframes`
-  0% {
+  \ 0% {
     -webkit-transform: translateY(-1000px);
       transform: translateY(-1000px);
     opacity: 0;
   }
-  100% {
+  \ 100% {
     -webkit-transform: translateY(0);
       transform: translateY(0);
     opacity: 1;
@@ -94,10 +94,10 @@ export const slideInTop = keyframes`
 `
 
 export const textAnimation = keyframes`
-  0% {
+  \ 0% {
     opacity: 0;
   }
-  100% {
+  \ 100% {
     opacity: 1;
   }
 `
