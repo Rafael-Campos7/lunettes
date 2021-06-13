baabf53aae49169c25c99686cc735b4e745a751d
Rafael Campos <rafaelscampos15@gmail.com>
2021-06-13 18:31:30 -0300

    Created the color selector component.

---
+++ src/components/ColorSelector/index.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import { Container, Color } from './styles/style'
+
+type Color = {
+  name: string;
+  background: string;
+}
+
+interface ColorSelectorProps {
+  onColorChange: (colorName: string) => void;
+  colors: Color[];
+}
+
+export function ColorSelector({ colors, onColorChange }: ColorSelectorProps) {
+  const [colorSelected, setColorSelected] = useState(colors[0].name)
+
+  onColorChange(colorSelected)
+
+  return (
+    <Container>
+      {colors.map((color) => {
+        return (
+          <Color 
+            key={color.name}
+            onClick={() => { setColorSelected(color.name) }}
+            background={color.background} 
+            active={color.name === colorSelected} 
+          >
+            {color.name}
+          </Color>
+        )
+      })}
+    </Container>
+  )
+}
